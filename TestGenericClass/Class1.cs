01f2dfc00ab435536554de44ccb695d7b778af20
MoizRazaBhutta <moiz_bhutta@hotmail.com>
2024-09-03 20:22:29 -0400

    Add project files.

---
+++ TestGenericClass/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace TestGenericClass
+{
+    public class GenericClass<T> where T:struct
+    {
+        private readonly ICollection<T> _collection;
+        public GenericClass()
+        {
+            _collection = new List<T>();
+        }
+        public void Add(T item)
+        {
+            _collection.Add(item);
+        }
+        public T GetItem(int index)
+        {
+            var list = _collection.ToList();
+            return list[index];
+        }
+
+        public List<T> SortDescending()
+        {
+            var list = _collection.ToList();
+            return list.OrderByDescending(w => w).ToList();
+        }
+    }
+}
